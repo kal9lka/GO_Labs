128a78780ae795258981bde41fe21710b58911ad
kal9lka <daniil.osipov38@gmail.com>
2024-09-18 19:57:07 +0400

    Add files via upload

---
+++ Lab_1/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
+
+func main() {
+	dataTime := time.Now()
+	fmt.Println("Current data is " + dataTime.Format("02-01-2006 15:04:05"))
+	fmt.Println("")
+
+	age := 21
+	name := "Daniil"
+	var PI float64 = 3.1415926
+	boolVar := true
+	fmt.Print("My name is: " + name + ". I am ")
+	fmt.Println(age)
+	fmt.Print("PI is ")
+	fmt.Println(PI)
+	fmt.Print("boolVar is ")
+	fmt.Println(boolVar)
+	fmt.Println("")
+
+	var a int
+	var b int
+	fmt.Println("a: ")
+	fmt.Scan(&a)
+	fmt.Println("b: ")
+	fmt.Scan(&b)
+	fmt.Print("Результат cуммы: ")
+	fmt.Println(a + b)
+	fmt.Println("")
+
+	var input string
+
+	fmt.Print("aFloat: ")
+	fmt.Scan(&input)
+	aFloat, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatalf("число указано не корректно: %v\n", err)
+	}
+
+	fmt.Print("bFloat: ")
+	fmt.Scan(&input)
+	bFloat, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		log.Fatalf("число указано не корректно: %v\n", err)
+	}
+
+	fmt.Println(aFloat)
+	fmt.Printf("Результат cуммы: %.2f\n", sumFloat(aFloat, bFloat))
+	fmt.Printf("Результат разности: %.2f\n", subFloat(aFloat, bFloat))
+	fmt.Println("")
+
+	var c int
+	fmt.Println("a: ")
+	fmt.Scan(&a)
+	fmt.Println("b: ")
+	fmt.Scan(&b)
+	fmt.Println("c: ")
+	fmt.Scan(&c)
+	fmt.Print("Среднее a, b, c: ")
+	fmt.Println(avg(a, b, c))
+}
+
+func sumFloat(a float64, b float64) float64 {
+	return a + b
+}
+func subFloat(a float64, b float64) float64 {
+	return a - b
+}
+
+func avg(a int, b int, c int) float64 {
+	result := float64((a + b + c) / 3)
+	return result
+}
